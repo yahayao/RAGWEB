38fe9a4126db338ddf1789afec86708630ca1067
yahayao <yahayao@126.com>
2026-03-17 20:12:08 +0800

    修复创建路由时的历史记录配置，恢复使用 import.meta.env.BASE_URL

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,7 +16,7 @@ const routes: RouteRecordRaw[] = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(), // 移除 import.meta.env.BASE_URL
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
 
